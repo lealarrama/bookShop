e061a137d0939ec5d611dec19a2150eb395ed02d
Lea Larrama <leandrolarrama@hotmail.com>
2021-11-21 17:31:04 -0300

    arreglos de multer

--- routes/products.js
+++ routes/products.js
@@ -9,10 +9,10 @@ const productController= require("../controllers/productController");
 // ************ Multer ************ 
 const storage = multer.diskStorage({
     destination: (req,file, callback)=>{
-        callback (null, path.join(__dirname,'../../public/img/products'))
+        callback (null, path.join(__dirname,'../public/img/products'))
     },
     filename: (req,file, callback)=>{
-        callback (null, file.fieldName + '-' + Date.now()+ path.extname(file.originalname))
+        callback (null, file.fieldname + '-' + Date.now()+ path.extname(file.originalname))
     }
 })
 var upload = multer({storage: storage})
@@ -20,15 +20,12 @@ var upload = multer({storage: storage})
 // /* get products page*/
 productRouter.get('/', productController.products);
 
-/* get productDetail page*/
-productRouter.get('/:id', productController.detail);
-
 /* get productCart page*/
 productRouter.get('/cart', productController.productCart);
 
 /* get createProduct page*/
 productRouter.get('/create', productController.createProduct);
-productRouter.post('/', upload.any(), productController.store);
+productRouter.post('/', upload.any('image'), productController.store);
 
 /* get editProduct page*/
 productRouter.get('/edit/:id', productController.editProduct);
@@ -37,6 +34,8 @@ productRouter.patch('/edit/:id', upload.any(), productController.update);
 /*** DELETE ONE PRODUCT***/ 
 productRouter.delete('/delete/:id', productController.destroy); 
 
+/* get productDetail page*/
+productRouter.get('/:id', productController.detail);
 
 
 module.exports = productRouter;
