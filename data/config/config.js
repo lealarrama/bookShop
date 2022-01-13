1629c7cf32820ee55c848d6fb942dc4a281f7997
AmandaZerpa <amanditazerpa@gmail.com>
2022-01-13 16:51:12 -0300

    .

--- data/config/config.js
+++ data/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password":Dakota,
+    "password": null,
     "database": "sk_libros",
     "host": "127.0.0.1",
     "dialect": "mysql",
